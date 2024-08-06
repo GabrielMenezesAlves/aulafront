da52a00543cd7bacbcbafe553afa9389fd095aff
Gabriel Menezes Alves <gabriel_cm15@hotmail.com>
2024-08-05 21:42:19 -0300

    javascript

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// comentario de 1 linha so
+/* comentario em varias linhas
+      ...
+      ... */
+// javascript nao e igual Java
+
+// var nome = prompt("Digite seu nome");
+
+// alert("Ola " + nome + "!!!");
+
+var nome = "Gabriel"; //texto -> string
+var idade = 23; //number -> integer
+var peso = 34.6; //number -> float
+var acordado = true; //boolean
+
+//exercicio
+//1 - diga quantos caracteres tem na palavra informada
+
+//. pegar a palavra digitada
+var palavra = prompt("digite uma palavra");
+//. contar os caracteres
+var quant = palavra.length;
+//. exibir os resultados
+alert("a quantidade de caracteres digitadas e: " + quant);
