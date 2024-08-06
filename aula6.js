e810fb3fe4f1cc53a2cc1f629efc3f5ab28d098b
Gabriel Menezes Alves <gabriel_cm15@hotmail.com>
2024-08-06 20:44:17 -0300

    exercicios

--- aula6.js
+++ aula6.js
@@ -2,5 +2,26 @@
 // console.log e somente pra programador...quando voce quer que o usuario veja, dai voce usar o ALERT por exemplo
 
 function oi() {
-  console.log(valor.value);
+  var entrada = valor.value;
+  saida.value = entrada;
+}
+
+// exercicios
+// exe1 - devolva o valor digitado de forma inversa ex: Gabriel = leirbaG
+// exe2 - coloque o nome todo em maiusculas, ex: Bola = BOLAS
+// exe3 - coloque a primeira letra em maiuscula, ex: bola = Bola
+
+function ex1() {}
+
+function ex2() {
+  var entrada = valor.value;
+  saida.value = entrada.toUpperCase();
+}
+
+function ex3() {
+  var entrada = valor.value;
+  var letra = entrada.charAt(0);
+  var resto = entrada.substr(1);
+
+  saida.value = letra.toUpperCase() + resto.toLowerCase();
 }
