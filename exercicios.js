b9e5a2055bfdddc891cca7e43f695d2aba633f56
Gabriel Menezes Alves <gabriel_cm15@hotmail.com>
2024-08-07 19:09:04 -0300

    exercicios

---
+++ exercicios.js
@@ -0,0 +1,44 @@
+//Exercicio 021
+//Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+
+//Exercicio 022
+//Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+function ex1() {
+    var entrada = valor.value;
+    saida.value = entrada;
+
+    
+    if (entrada = f) {
+        saida.value = "Feminino";
+    } if (entrada = m) {
+        saida.value = "Masculino";
+    } else {
+        saida.value = "Outro";
+    }
+
+}
+
+function ex2() {
+    var entrada = valor.value;
+    saida.value = entrada;
+
+    if (entrada = a){
+        saida.value = "vogal";
+    }
+    if (entrada = e){
+        saida.value = "vogal";
+    }
+    if (entrada = i){
+        saida.value = "vogal";
+    }
+    if (entrada = o){
+        saida.value = "vogal";
+    }
+    if (entrada = u){
+        saida.value = "vogal";
+    } else {
+        saida.value = "consoante";
+    }
+
+}
