e848467873d40aac709c6ed8829263cef20c370f
Gabriel Menezes Alves <gabriel_cm15@hotmail.com>
2024-08-06 21:49:31 -0300

    if else

--- aula6.js
+++ aula6.js
@@ -10,6 +10,10 @@ function oi() {
 // exe1 - devolva o valor digitado de forma inversa ex: Gabriel = leirbaG
 // exe2 - coloque o nome todo em maiusculas, ex: Bola = BOLAS
 // exe3 - coloque a primeira letra em maiuscula, ex: bola = Bola
+// exe4 - some o numero 1 + numero 2
+// exe5 - calculo do IMC, pedir peso e altura e devolver o resultado: peso/alturaxaltura ( + soma - subtrai / divisao * multiplica)
+// exe6 - pegue 2 numeros e diga qual deles e o maior.
+// exe7 - pegue a idade da pessoa e diga se e crianca, adulto ou idoso
 
 function ex1() {}
 
@@ -24,4 +28,55 @@ function ex3() {
   var resto = entrada.substr(1);
 
   saida.value = letra.toUpperCase() + resto.toLowerCase();
+}
+
+function ex4() {
+  var n1 = num1.value;
+  var n2 = num2.value;
+
+  saida.value = parseInt(n1) + parseInt(n2);
+}
+
+// outra forma ex5
+// var n1 = num1.value;
+// var n2 = num2.value;
+// n1 = parseFloat(n1);
+// n2 = parseFloat(n2);
+// var res = n1 / (n2 * n2);
+// saida.value = res;
+
+function ex5() {
+  var peso = num1.value;
+  var altura = num2.value;
+
+  saida.value = parseFloat(peso) / parseFloat(altura * altura);
+}
+
+function ex6() {
+  var n1 = num1.value;
+  var n2 = num2.value;
+
+  n1 = parseInt(n1);
+  n2 = parseInt(n2);
+
+  if (n1 > n2) {
+    saida.value = n1;
+  } else {
+    saida.value = n2;
+  }
+}
+
+function ex7() {
+  var n1 = num1.value;
+
+  n1 = parseInt(n1);
+
+  // a ordem da codificacao altera o resultado, a logica tem que existir, pro programa nao bugar.
+  if (n1 >= 65) {
+    saida.value = "idoso";
+  } else if (n1 >= 18) {
+    saida.value = "adulto";
+  } else {
+    saida.value = "crianca";
+  }
 }
